52aeee6298255f41cbdb31a48d315e5f4cf91537
Justin Green <justingreen@Justins-MacBook-Air.local>
2024-01-23 19:27:28 -0800

    save

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -7,7 +7,8 @@ import {useGLTF, Float,Box,useAnimations} from "@react-three/drei"
 const Model = ()=>{
     const img = useGLTF("./models/typingguy2.glb")
     const modelRef = useRef();
-    const pos = [0,-2.5,-1];
+    const [pos,setPos] = useState([0,-2.5,-1]);
+    const [scale,setScale] = useState(.6)
     const {actions,mixer} = useAnimations(img.animations,img.scene)
     const [showIntro,setShowIntro] = useState(true)
 
@@ -19,15 +20,17 @@ const Model = ()=>{
     useEffect(()=>{
       if(innerWidth < 950){
         //
+        setScale(1)
+        setPos([.5,-2.5,-2])
         console.log('mobile size...')
       }
 
-      setTimeout(()=>{
-        console.log('stop intro!')
+      // setTimeout(()=>{
+      //   console.log('stop intro!')
 
-        setShowIntro(showIntro=>showIntro = false)
-        mixer.stopAllAction();
-      },5500);
+      //   setShowIntro(showIntro=>showIntro = false)
+      //   mixer.stopAllAction();
+      // },5500);
       
     },[])
 
@@ -36,29 +39,36 @@ const Model = ()=>{
     useFrame(()=>{
       img.animations.forEach(animation=>{
       //   if(showIntro){
-      //   if(animation.name != "Typing"){
+      //   if(animation.name == "ChairAction"){
       //    let action = mixer.clipAction(animation)
       //    action.play();
-      //   //  console.log("typing...")
+      //     // console.log("typing...")
       //   }
       // }
       // else{
         if(animation.name == "Typing"){
           let action = mixer.clipAction(animation)
           action.play();
-         //  console.log("typing...")
          }
-      // }
+        //   if(showIntro){
+        //    if(animation.name == "LowerCurtain"){
+        //     let action = mixer.clipAction(animation)
+        //     action.play();
+        //    }
+        //  }
+      //  }
        })
     })
 
 
     return (
-      <group>
-        {/* <directionalLight position={[0,0,1]} intensity={2}/>
-        <pointLight intensity={3} position={[0,1,-1]} /> */}
+      <group onClick={()=>{
+
+      }}>
+         {/* <directionalLight position={[0,0,1]} intensity={2}/> */}
+        {/* <pointLight intensity={3} position={[0,1,-1]} />  */}
         <Float>
-        <primitive scale={.6} position={[0,-2.5,-1]} rotation={[0,Math.PI * -.1,0]} object={img.scene}/>
+        <primitive scale={scale} position={pos} rotation={[0,Math.PI * -.3,0]} object={img.scene}/>
         </Float>
       </group>
     )
